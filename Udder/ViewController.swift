a52056a898de7f2e5d95f42cced4b89edf7a5530
Dislik <dislik2013@gmail.com>
2019-03-24 09:50:05 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Irina Makarova on 24.03.2019.
+//  Copyright © 2019 Irina Makarova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
